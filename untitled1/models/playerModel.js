5b9315371b6724da49281eac8e5bfd4937e27482
YounesAmraoui <48179472+YounesAmraoui@users.noreply.github.com>
2021-03-22 15:12:42 +0100

    player model modifie

--- untitled1/models/playerModel.js
+++ untitled1/models/playerModel.js
@@ -2,6 +2,7 @@ var playerSchema = mongoose.Schema({
     // Using local for Local Strategy Passport
 
         name: String,
+        prenom: String,
         age: Number,
         nationality: String,
         image: String,
